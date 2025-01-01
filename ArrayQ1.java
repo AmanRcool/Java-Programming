a0ffa83c21ae1f54619d697ff2d4164dec4f61ec
AmanRcool <141349097+AmanRcool@users.noreply.github.com>
2025-01-01 19:12:57 +0530

    define size of array and taking input from user and display the output taken by user

---
+++ ArrayQ1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class ArrayQ1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the Elements of Array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
